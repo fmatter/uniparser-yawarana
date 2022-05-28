bf5b4732704b3b36510427be8f8054e48d9d9812
Florian Matter <florianmatter@gmail.com>
2022-05-28 15:11:24 -0700

    uncle test is back

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -19,24 +19,31 @@ def test_multiple(parser):
 def test_bad_parsing(parser):
     assert len(parser.analyze_words("yawë", parser)) == 1
 
+
 def test_noun_inflection(parser):
-    for x in ["uyïwïj", "mëyïwïj", "tïwïj"]: # 'house'
+    for x in ["uyïwïj", "mëyïwïj", "tïwïj"]:  # 'house'
         form = parse_1(x, parser)
         assert form.lemma == "ïwïtï"
         assert "house" in form.gloss
         assert "poss" in form.gramm
 
-    for x in ["umukuru", "mëmukuru", "imukuru"]: # 'child'
+    for x in ["umukuru", "mëmukuru", "imukuru"]:  # 'child'
         form = parse_1(x, parser)
         assert form.lemma == "muku"
         assert "child" in form.gloss
         assert "poss" in form.gramm
 
-    for x in ["unajmori", "anajmori", "inajmori"]: # 'grandmother'
+    for x in ["unajmori", "anajmori", "inajmori"]:  # 'grandmother'
         form = parse_1(x, parser)
         assert form.lemma == "najmo"
         assert "grand.mother" in form.gloss
         assert "poss" in form.gramm
+
+    for x in ["yëri", "akajrari", ""]:  # 'uncle / father in law'
+        form = parse_1(x, parser)
+        # assert form.lemma == "najmo"
+        # assert "grand.mother" in form.gloss
+        # assert "poss" in form.gramm
 
     assert "pert" in parse_1("yëri", parser).gramm
     assert "NPERT" in parse_1("yëtë", parser).gloss
