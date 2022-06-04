c9fdded8d7209d5ddd53f8825932e523324e16dc
Florian Matter <florianmatter@gmail.com>
2022-06-04 10:38:36 -0700

    work on derivations

---
+++ tests/test_derivations.py
@@ -0,0 +1,57 @@
+def parse_1(str, parser):
+    return parser.analyze_words(str)[0]
+
+
+def eliminate_analysis(anas, bad_glosses):
+    for ana in anas:
+        for gloss in bad_glosses:
+            if gloss in ana.gloss:
+                continue
+        return ana
+
+
+def pick_analysis(anas, good_gloss):
+    for ana in anas:
+        if good_gloss in ana.gloss:
+            return ana
+    return None
+
+
+def run_polysemy_test(forms, parser):
+    for form, data in forms:
+        analyses = parser.analyze_words(form)
+        print(analyses)
+        for gloss, tag in data.items():
+            analysis = pick_analysis(analyses, gloss)
+            assert tag in analysis.gramm
+
+
+def test_tojpe(parser):
+    run_polysemy_test(
+        forms=[
+            ("ijmokatojpe", {"PURP": "adv", "FUT": "vt"}),
+            ("tawejkatojpe", {"PURP": "adv", "FUT": "vi"}),
+        ],
+        parser=parser,
+    )
+
+
+def test_ri(parser):
+    run_polysemy_test(
+        forms=[
+            ("yaruwari", {"NMLZ": "n", "IPFV": "vi"}),  # intransitive 'laugh'
+            ("iniri", {"NMLZ": "n", "IPFV": "vt"}),  # transitive 'see'
+            ("ini", {"NMLZ": "n", "IPFV": "vt"}),  # transitive 'see'
+        ],
+        parser=parser,
+    )
+
+def test_ptcp(parser):
+    run_polysemy_test(
+        forms=[
+            ("wïnïjse", {"SUP": "adv", "PTCP": "adv", "PST": "vi"}),  # intransitive 'sleep'
+            ("këyamase", {"SUP": "adv", "PTCP": "adv", "PST": "vt"}),  # transitive 'create'
+            ("inche", {"SUP": "adv", "PTCP": "adv", "PST": "vt"}),  # transitive 'see'
+        ],
+        parser=parser,
+    )
